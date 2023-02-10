b88be2281dab8d7aed8e7b8685796b3f5bef6e45
Radu Maier <ariademur@webserv.co>
2023-02-10 18:21:59 +0100

    bin/cli

--- phan/config.php
+++ phan/config.php
@@ -350,6 +350,8 @@ return [
     // Thus, both first-party and third-party code being used by
     // your application should be included in this list.
     'directory_list' => [
+        'bin',
+        'cli',
         'config',
         'public',
         'resources',
